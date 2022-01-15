0a5dc7f70a12a28b87f73052009050e6ab9ddfcf
ZakSchenck <91504668+ZakSchenck@users.noreply.github.com>
2022-01-14 21:08:53 -0500

    Changed padding to 20px on VR img

--- landing.js
+++ landing.js
@@ -1,6 +1,7 @@
 let hamburger = document.querySelector('.container');
 let menu = document.querySelector('.menu-item-container');
+let header = document.querySelector('.header-container')
 hamburger.addEventListener('click', function() {
-    menu.classList.toggle('show')
+    menu.classList.toggle('show');
 })
 
