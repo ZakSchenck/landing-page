c7581f83810b4a6c8eadbd28c7abb516ffc86c8c
ZakSchenck <91504668+ZakSchenck@users.noreply.github.com>
2022-01-14 20:48:22 -0500

    Initial commit

---
+++ landing.js
@@ -0,0 +1,6 @@
+let hamburger = document.querySelector('.container');
+let menu = document.querySelector('.menu-item-container');
+hamburger.addEventListener('click', function() {
+    menu.classList.toggle('show')
+})
+
